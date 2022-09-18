a4d09117a7f8cbfd757bd43f24fd3aa66f34f381
Adam The Canadian <adam.jarrett.clare@gmail.com>
2022-09-18 09:03:10 -0600

    Enabling O2 optimization and benchmarking across 5 point clouds

--- benchmarks/scan_registration_benchmarks.cpp
+++ benchmarks/scan_registration_benchmarks.cpp
@@ -12,7 +12,6 @@ static pcl::PointCloud<pcl::PointXYZI> point_cloud;
 static void BM_ScanRegistration(benchmark::State& state) {
   Log::LoggingLevel() = logging::LogLevel::Error;
   int binary_file_num = (int)state.range(0);
-  std::cout << "binary_file_num " << binary_file_num << std::endl;
   point_cloud = mloam::LoadKittiData("/Users/adamclare/data/2011_09_30/2011_09_30_drive_0028_sync/velodyne_points/data/", binary_file_num);
 
   while (state.KeepRunning()) {
@@ -25,6 +24,6 @@ static void BM_ScanRegistration(benchmark::State& state) {
   }
 }
 // Register the function as a benchmark
-BENCHMARK(BM_ScanRegistration)->DenseRange(0, 2, 1);
+BENCHMARK(BM_ScanRegistration)->DenseRange(0, 5, 1);
 
 BENCHMARK_MAIN();
